efa408b088f8b675e65d3a3a14e04415dbb564d5
Yury Lavrukhin <yurka7321@gmail.com>
2018-08-19 11:46:06 +0600

    Добавлен функционал слайдера

--- src/js/modules/scripts-arrows.js
+++ src/js/modules/scripts-arrows.js
@@ -1,16 +1,40 @@
 const leftArrow = document.querySelector('.content-scripts__arrow--left');
 const rightArrow = document.querySelector('.content-scripts__arrow--right');
 
-const panelLists = document.querySelectorAll('.content-scripts .panel__list');
-const panelItems = document.querySelectorAll('.content-scripts .panel__list .panel__item');
+const panelLists = document.querySelectorAll('.content-scripts__panel-list');
+const panelsContainer = document.querySelector('.content-scripts__panels-container');
 
-if (panelLists.length > 1) {
+if (panelLists.length > 1) { // Если больше одного слайда
+  // Показываем стрелки
   leftArrow.classList.add('show');
   rightArrow.classList.add('show');
 
-  rightArrow.addEventListener('click', () => {
-    panelLists[0].classList.add('active');
+  // Добавляем класс active к первому слайду
+  panelLists[0].classList.add('active');
+
+  let position = 0;
+  let activeList;
+  let panelListWidth = panelLists[0].offsetWidth;
+
+  leftArrow.addEventListener('click', () => {
+    activeList = document.querySelector('.content-scripts__panel-list.active');
+    if (activeList.previousElementSibling) {
+      position += panelListWidth;
+      panelsContainer.style.transform = 'translateX(' + position + 'px)';
+      activeList.classList.remove('active');
+      activeList.previousElementSibling.classList.add('active');
+    }
   });
-
-
+  
+  rightArrow.addEventListener('click', () => {
+    activeList = document.querySelector('.content-scripts__panel-list.active');
+    if (activeList.nextElementSibling) {
+      position += -panelListWidth;
+      panelsContainer.style.transform = 'translateX(' + position + 'px)';
+      activeList.classList.remove('active');
+      activeList.nextElementSibling.classList.add('active');
+    }
+  });
 };
+
+
