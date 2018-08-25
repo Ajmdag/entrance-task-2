5f2a3fec9fae0c0d48d95a33b588f649b6b0ad35
Yury Lavrukhin <yurka7321@gmail.com>
2018-08-25 19:47:38 +0600

    Добавление возможности закрыть меню фильтра девайсов при клике вне этого меню

--- src/js/modules/devices-open-menu.js
+++ src/js/modules/devices-open-menu.js
@@ -4,3 +4,15 @@ let menuList = document.querySelector('.content-devices__menu-list');
 itemClickToShow.addEventListener('click', () => {
   menuList.classList.toggle('open');
 });
+
+document.addEventListener('click', event => {
+  if (menuList.classList.contains('open')
+    && !event.target.classList.contains('content-devices__menu-list-item')
+    && !event.target.classList.contains('content-devices__checkbox')
+    && !event.target.classList.contains('content-devices__menu-list-item-all')
+    && !event.target.parentNode.classList.contains('content-devices__menu-list-item-all')) {
+      menuList.classList.remove('open');
+      console.log('somehitns')
+      console.log(event.target)
+  }
+});
