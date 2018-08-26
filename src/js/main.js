d7e02c35db3b8e63feb6d21b28fbe3ffdae88da4
Yury Lavrukhin <yurka7321@gmail.com>
2018-08-26 16:45:45 +0600

    Добавление скролла на стрелки для девайсов

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
-import {} from './modules/devices-popup';
 import {} from './modules/devices-filter';
 import {} from './modules/devices-open-menu';
+import {} from './modules/devices-popup';
+import {} from './modules/devices-scroll';
 import {} from './modules/main-scroll-panels';
 import {} from './modules/scripts-arrows';
