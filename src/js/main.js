691b9db6ae76f022eb04a8ba8eba6a4453b154b0
Yury Lavrukhin <yurka7321@gmail.com>
2018-08-26 14:15:27 +0600

    Показ меню в хедере через чекбокс, футер к низу странице на десктопе

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
 import {} from './modules/devices-popup';
 import {} from './modules/devices-filter';
 import {} from './modules/devices-open-menu';
-import {} from './modules/header-nav';
 import {} from './modules/main-scroll-panels';
 import {} from './modules/scripts-arrows';
--- src/js/modules/header-nav.js
+++
@@ -1,7 +0,0 @@
-const nav = document.querySelector('.header__nav');
-const burger = document.querySelector('.header__burger');
-
-burger.addEventListener('click', () => {
-  nav.classList.toggle('show');
-  burger.classList.toggle('close');
-});
